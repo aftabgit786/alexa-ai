ee2c586ecf7aa5f949b511151627f0534ad5b2c1
aftabgit786 <66480543+aftabgit786@users.noreply.github.com>
2023-02-28 11:06:51 +0500

    update alexa

--- alexa.py
+++ alexa.py
@@ -17,6 +17,7 @@ def talk(text):
 
 
 def take_command():
+    command = ""  # initialize command to an empty string
     try:
         with sr.Microphone() as source:
             print('listening...')
@@ -32,29 +33,30 @@ def take_command():
 
 
 def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    else:
-        talk('Please say the command again.')
+    while True:  # add loop to keep listening for new commands
+        command = take_command()
+        print(command)
+        if 'play' in command:
+            song = command.replace('play', '')
+            talk('playing ' + song)
+            pywhatkit.playonyt(song)
+        elif 'time' in command:
+            time = datetime.datetime.now().strftime('%I:%M %p')
+            talk('Current time is ' + time)
+        elif 'who the heck is' in command:
+            person = command.replace('who the heck is', '')
+            info = wikipedia.summary(person, 1)
+            print(info)
+            talk(info)
+        elif 'date' in command:
+            talk('sorry, I have a headache')
+        elif 'are you single' in command:
+            talk('I am in a relationship with wifi')
+        elif 'joke' in command:
+            talk(pyjokes.get_joke())
+        else:
+            talk('Please say the command again.')
 
 
-while True:
+if __name__ == "__main__":  # indent last three lines of code
     run_alexa()
